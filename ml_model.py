841ad2f93ac0dbc86a671e7f1785fa9f2cbc2165
rushikesh5555 <pawarrushi5555@gmail.com>
2023-06-20 22:34:31 +0530

    added new ml model code

---
+++ ml_model.py
@@ -0,0 +1,10 @@
+print("adding ML related code here")
+
+def model1():
+    print("adding code for model 1")
+
+def model12():
+    print("adding code for model 2")
+
+def model3():
+    print("adding code for model 3")
